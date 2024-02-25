0da28f97fa9f8b53b808fe0032f73c557fd2b919
Kai Harries <kai.harries@posteo.de>
2024-02-25 08:59:33 +0100

    Allow ssh login by root

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,11 @@
 { config, pkgs, ... }: with pkgs;
 let
   myKernelPackages = pkgs.linuxPackagesFor linux_latest;
+  mySshKeys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKeT9XLuhzUU4k4gd8URDS3gQIZemTqXSvlVy5nYXJ4gMfJ0sYVMrI9KBBU2Ukkb0Cl8Rmfzblf1iE6IUMrat4Cb9RGIbzjiAzC2XaLUsDC5W87Qv5bgV0t83nWQFjWPWy38Ybjcp8+WuvJNaX9ECc8t+xwtUdVNZ5TszblEqE5wKfOAqJZNGO8uwX2ZY7hOLr9C9a/AM74ouHqR7iDaujMNdLuOA6XmHAnWI6aiA6Lu3NOpGO6UXIudUCIUQ+ymSCCfu99xaAs5aXw/XQLS2f8W8C4q45m/V+uozdqYOK2wrFQlhFa/7TZwi5s3XPeG0d7t5HnxymSIHO7HudP0E7 cardno:00050000351F"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrph3lPTTICQR4SgX5X2XXTVcPqUmeS9urKJjCLS92rFycxJFgUNHq5Wwjctq1dwsNI7l7+td6kzaz5SsIfNLlFpEoiS3kfG/mRHlkJYOuiCpofI3AIDnHdNDRHhztf+n2i/s3758EyRu0Ct30QwMP+aVSOqA+NFpeKyJdSnL+PwVWPdi2HooUdNbBR2QtB/qQ39DmzB7BqtUWSOUB4EgoKnrfLfN03z1L4XOQCKtzrqar3oGYXvZ0K5nIOGktByd8TXecH1+DCE0tdTXvJoobj+fWvin3FmjjiitK05b7+pa7GadzmmnvgtrLLTY/CUSB/NNCs2k6j22woYNQcxeLHfifkMNFFBOXQKUHX5iw3RJAwbY92XKN6t8VyvHPv5qLXMo4NFhWjTRKgY5VWNn94sr1ZQ8RV/teUr3TnZTMyNo0a13BGtdKUGHppRSPEQQ3VZbureIvfGMcO7NQvloXdLjvFJyR9E+NX1MVZyLThsZkCgWLB/AIq+8Gl9myfe0= kai@nix230"
+  ];
+
 in {
   imports = [
     <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
@@ -76,10 +81,8 @@ in {
       "networkmanager"
       "wheel"
     ];
-  users.users.kai.openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCKeT9XLuhzUU4k4gd8URDS3gQIZemTqXSvlVy5nYXJ4gMfJ0sYVMrI9KBBU2Ukkb0Cl8Rmfzblf1iE6IUMrat4Cb9RGIbzjiAzC2XaLUsDC5W87Qv5bgV0t83nWQFjWPWy38Ybjcp8+WuvJNaX9ECc8t+xwtUdVNZ5TszblEqE5wKfOAqJZNGO8uwX2ZY7hOLr9C9a/AM74ouHqR7iDaujMNdLuOA6XmHAnWI6aiA6Lu3NOpGO6UXIudUCIUQ+ymSCCfu99xaAs5aXw/XQLS2f8W8C4q45m/V+uozdqYOK2wrFQlhFa/7TZwi5s3XPeG0d7t5HnxymSIHO7HudP0E7 cardno:00050000351F"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrph3lPTTICQR4SgX5X2XXTVcPqUmeS9urKJjCLS92rFycxJFgUNHq5Wwjctq1dwsNI7l7+td6kzaz5SsIfNLlFpEoiS3kfG/mRHlkJYOuiCpofI3AIDnHdNDRHhztf+n2i/s3758EyRu0Ct30QwMP+aVSOqA+NFpeKyJdSnL+PwVWPdi2HooUdNbBR2QtB/qQ39DmzB7BqtUWSOUB4EgoKnrfLfN03z1L4XOQCKtzrqar3oGYXvZ0K5nIOGktByd8TXecH1+DCE0tdTXvJoobj+fWvin3FmjjiitK05b7+pa7GadzmmnvgtrLLTY/CUSB/NNCs2k6j22woYNQcxeLHfifkMNFFBOXQKUHX5iw3RJAwbY92XKN6t8VyvHPv5qLXMo4NFhWjTRKgY5VWNn94sr1ZQ8RV/teUr3TnZTMyNo0a13BGtdKUGHppRSPEQQ3VZbureIvfGMcO7NQvloXdLjvFJyR9E+NX1MVZyLThsZkCgWLB/AIq+8Gl9myfe0= kai@nix230"
-  ];
+  users.users.kai.openssh.authorizedKeys.keys = mySshKeys;
+  users.users.root.openssh.authorizedKeys.keys = mySshKeys;
 
   security.sudo.extraConfig =''kai ALL = NOPASSWD : ALL'';
 
@@ -87,7 +90,6 @@ in {
   services.openssh.enable = true;
   services.openssh.settings.KbdInteractiveAuthentication = false;
   services.openssh.settings.PasswordAuthentication = false;
-  services.openssh.settings.PermitRootLogin = "no";
 
   environment.homeBinInPath = true;
   environment.etc."inputrc".text = lib.mkForce (
